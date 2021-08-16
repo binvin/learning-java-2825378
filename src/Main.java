62c1718ffd74931df55b86fcebb09436eccabbe9
binvin <vinobindu@gmail.com>
2021-08-16 12:16:16 -0400

    Update Main.java
    My version

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,47 @@
 public class Main {
-
-    public static double salaryCalculator(double hoursPerWeek,
-                                          double amountPerHour,
-                                          int vacationDays) {
-        if (hoursPerWeek < 0) {
-            return -1;
+    public static double calcEmpSalary(int empHrsPerWeek, double empRatePerHour, double vacationDays){
+        if (empHrsPerWeek < 0){
+            System.out.println("Hours parameter is negative!");
+            return 0;
+        } else if (empRatePerHour < 0){
+            System.out.println("Rate parameter is negative!");
+            return 0;
+        } else if (vacationDays < 0){
+            System.out.println("Vacation parameter is negative!");
+            return 0;
         }
 
-        if (amountPerHour < 0) {
-            return -1;
+        double empYearlySalary = (empHrsPerWeek * empRatePerHour * 52.1429)
+                - (vacationDays * empHrsPerWeek/5 * empRatePerHour);
+        return empYearlySalary;
+    }
+    public static void main(String[] args){
+        int empHrsPerWeek = 40;
+        double empRatePerHour = 70;
+        double vacationDays = 0;
+        double empGrossSalary = calcEmpSalary(empHrsPerWeek, empRatePerHour, vacationDays);
+        System.out.println("Employee gross salary with " + empHrsPerWeek + " hrs, and rate $"
+                + empRatePerHour + " , with " + vacationDays + " vacation days is : $"
+                + empGrossSalary);
+
+        empHrsPerWeek = 40;
+        empRatePerHour = 70;
+        vacationDays = 1;
+        empGrossSalary = calcEmpSalary(empHrsPerWeek, empRatePerHour, vacationDays);
+        System.out.println("Employee gross salary with " + empHrsPerWeek + " hrs, and rate $"
+                + empRatePerHour + " , with " + vacationDays + " vacation days is : $"
+                + empGrossSalary);
+
+        empHrsPerWeek = 40;
+        empRatePerHour = 70;
+        vacationDays = -1;
+        empGrossSalary = calcEmpSalary(empHrsPerWeek, empRatePerHour, vacationDays);
+        if (empGrossSalary > 0) {
+            System.out.println("Employee gross salary with " + empHrsPerWeek + " hrs, and rate $"
+                    + empRatePerHour + " , with " + vacationDays + " vacation days is : $"
+                    + empGrossSalary);
+        } else {
+            System.out.println("Error!!!");
         }
-
-        double weeklyPaycheck = hoursPerWeek * amountPerHour;
-        double unpaidTime = vacationDays * amountPerHour * 8;
-        return (weeklyPaycheck * 52) - unpaidTime;
     }
-
-    public static void main(String[] args) {
-        double salary = salaryCalculator(40, 15, 8);
-        System.out.println(salary);
-    }
-
 }
