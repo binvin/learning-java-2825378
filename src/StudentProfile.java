5fb16e567acc3a3c487e375c0dd1bdc493f77ede
binvin <vinobindu@gmail.com>
2021-08-18 15:27:08 -0400

    Update StudentProfile.java
    My version

--- src/StudentProfile.java
+++ src/StudentProfile.java
@@ -1,20 +1,34 @@
+import java.util.Calendar;
+
 public class StudentProfile {
     String firstName;
     String lastName;
-    String declaredMajor;
-    double gpa;
-    int expectedYearToGraduate;
+    int expectedGradYear;
+    int credits;
+    double GPA;
+    String major;
 
     public StudentProfile(String firstName, String lastName,
-                          String declaredMajor, double gpa, int expectedYearToGraduate) {
+                          int expectedGradYear, double GPA,
+                          String major, int credits){
         this.firstName = firstName;
         this.lastName = lastName;
-        this.declaredMajor = declaredMajor;
-        this.gpa = gpa;
-        this.expectedYearToGraduate = expectedYearToGraduate;
+        this.expectedGradYear = expectedGradYear;
+        this.GPA = GPA;
+        this.major = major;
+        this.credits = credits;
     }
 
-    public void incrementExpectedGraduationYear() {
-        this.expectedYearToGraduate = this.expectedYearToGraduate + 1;
+    public void incrementExpectedGradYear(){
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        if (this.credits > 90) {
+            this.expectedGradYear = year + 1;
+        } else if (this.credits > 60) {
+            this.expectedGradYear = year + 2;
+        } else if (this.credits > 30) {
+            this.expectedGradYear = year + 3;
+        } else {
+            this.expectedGradYear = year + 4;
+        }
     }
 }
